8c65cabaca4ce4d83dc034702f754a7b40c0664f
J.W. Clark <jwclark@rockhursths.edu>
2013-03-11 13:59:38 -0500

    work in progress for iteration 1

--- Strikethrough/Members/Default.aspx
+++ Strikethrough/Members/Default.aspx
@@ -1,8 +1,8 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Assets/MasterPages/Member.master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Strikethrough.Members.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MemberPlaceholder" runat="server">
-    <asp:Button ID="Button1" runat="server" Text="Button" />
-    <br />
-    <asp:Button ID="Button2" runat="server" Text="Button" />
-    <br />
-    <asp:Button ID="Button3" runat="server" Text="Button" />
+    <asp:Button ID="btnCreate" runat="server" Text="Button" OnClick="btnCreate_Click" />
+    <asp:DropDownList ID="ddlOpen" runat="server"></asp:DropDownList>
+    <asp:SqlDataSource ID="dsCanvas" runat="server"></asp:SqlDataSource>
 </asp:Content>
+
+
--- Strikethrough/Members/Default.aspx.designer.cs
+++ Strikethrough/Members/Default.aspx.designer.cs
@@ -13,30 +13,30 @@ namespace Strikethrough.Members {
     public partial class Default {
         
         /// <summary>
-        /// Button1 control.
+        /// btnCreate control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.Button btnCreate;
         
         /// <summary>
-        /// Button2 control.
+        /// ddlOpen control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.DropDownList ddlOpen;
         
         /// <summary>
-        /// Button3 control.
+        /// dsCanvas control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button3;
+        protected global::System.Web.UI.WebControls.SqlDataSource dsCanvas;
     }
 }
