06667de7c5ec1117d9e2d1f169ff6b457f2516df
JamesWClark <jwclark@rockhursths.edu>
2013-03-12 23:37:10 -0500

    iteration 1 complete

---
+++ Strikethrough/Developers/NUnit/Test-WebService-DataHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using NUnit.Framework;
+
+namespace Strikethrough.Developers.NUnit
+{
+    [TestFixture]
+    public class Test_WebService_DataHandler
+    {
+
+
+        [SetUp]
+        public void Init()
+        {
+        }
+        [Test]
+        public void IsResultDataTable()
+        {
+            string query = "SELECT * FROM aspnet_Users";
+            Assets.WebServices.DataHandler dataHandler = new Assets.WebServices.DataHandler();
+            DataTable dt = new DataTable();
+            Assert.AreEqual(dt.GetType(), dataHandler.GetDataTable(query).GetType());
+        }
+
+    }
+}
