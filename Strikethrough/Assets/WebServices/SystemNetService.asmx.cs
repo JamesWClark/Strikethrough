132dcfdd6f3dbaa9001b3149785d69c7d4e9426e
JamesWClark <jwclark@rockhursths.edu>
2013-03-27 00:03:22 -0500

    redo
    !@#$ github

---
+++ Strikethrough/Assets/WebServices/SystemNetService.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Net;
+
+namespace Strikethrough.Assets.WebServices
+{
+    /// <summary>
+    /// Summary description for SystemNetService
+    /// </summary>
+    [WebService(Namespace = "vhost0165.site1.compute.ihost.com")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class SystemNetService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
